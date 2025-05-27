5651ff8b1d4348f13dc5c51bc087179dbf827a03
chandrashekar Gj <chandrashekargj27@gmail.com>
2025-05-27 22:15:03 +0530

    Day 1. Project Created

---
+++ src/main/java/com/jnana/JnanaApplication.java
@@ -0,0 +1,13 @@
+package com.jnana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnanaApplication.class, args);
+	}
+
+}
